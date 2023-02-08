2f44acca986cfde8d9ff11e0e49f727a6c0aa09a
Wisnu Murfadilah <wisnukm41@gmail.com>
2023-02-08 12:50:17 +0700

    Add merge two list (Easy) I dont know how?!

---
+++ 21-merge-two-list/index.js
@@ -0,0 +1,49 @@
+// Other People Solution, I dont know about list in JS
+/**
+ * Definition for singly-linked list.
+
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+
+var mergeTwoLists = function (list1, list2) {
+  let head = new ListNode(),
+    tail;
+  if (!list1 || !list2) return list2 || list1;
+
+  if (list1.val < list2.val) {
+    head = list1;
+    list1 = list1.next;
+  } else {
+    head = list2;
+    list2 = list2.next;
+  }
+  tail = head;
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      tail.next = list1;
+      tail = tail.next;
+      list1 = list1.next;
+    } else {
+      tail.next = list2;
+      tail = tail.next;
+      list2 = list2.next;
+    }
+  }
+  tail.next = list1 || list2;
+
+  return head;
+};
+
+let list1 = [1, 2, 4];
+let list2 = [1, 3, 4];
+
+mergeTwoLists(list1, list2);
